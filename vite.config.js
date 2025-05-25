d3cb8137d029e480ed2bc006618e0ae29b53b30d
OreoIsCoding <axeldionisio0103@gmail.com>
2025-05-26 06:27:04 +0800

    feat: add React dependency and update axios configuration
    - Added React dependency in .vite/deps/react.js and its source map.
    - Updated axios configuration in public/axios/axios.js to use consistent double quotes and added "X-Requested-With" header.
    - Modified index.css to import Google Fonts and set Poppins as the default font family.
    - Cleaned up main.jsx by ensuring consistent import statements and formatting.
    - Removed unnecessary proxy configuration from vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
       base: '/manga-web/',
-
-  plugins: [react(),tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.mangadex.org',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+ plugins: [react(),tailwindcss()],
 })
