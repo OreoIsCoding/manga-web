cdb9451a263d62dbc5da631275e1ee23e7c51fd0
OreoIsCoding <axeldionisio0103@gmail.com>
2025-05-26 16:57:57 +0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-      base: '/manga-web/',
- plugins: [react(),tailwindcss()],
+  base: '/',
+  plugins: [react(), tailwindcss()],
 })
